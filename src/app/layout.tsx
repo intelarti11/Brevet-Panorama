0fc4a9ee5a60ba2fc5891add82b05c5660a2f70e
Florent Maboite <maboiteflorent@gmail.com>
2025-06-17 12:00:42 +0000

    change le nom pour "Brevet Panorama"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'BrevetSynthèse',
+  title: 'Brevet Panorama',
   description: 'Manage student exam results efficiently.',
 };
 
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,7 +7,7 @@ interface LogoProps {
 const Logo: FC<LogoProps> = ({ className }) => {
   return (
     <div className={`font-headline text-3xl font-bold text-primary ${className}`}>
-      BrevetSynthèse
+      Brevet Panorama
     </div>
   );
 };
